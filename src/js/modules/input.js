4707c5504b512261369ccc453ff6bfe223e57aed
username <user15b12n5lkuidso2@gmail.com>
2023-03-08 20:12:36 +0200

    modified:   dist/assets/css/style.min.css 	modified:   dist/assets/js/app.min.js 	modified:   dist/index.html 	modified:   gulp/version.json 	modified:   src/html/footer.html 	modified:   src/html/head.html 	modified:   src/html/header.html 	modified:   src/index.html 	modified:   src/js/modules/input.js 	modified:   src/js/modules/menu.js 	modified:   src/scss/base/other.scss 	modified:   src/scss/base/variebles.scss 	modified:   src/scss/blocks/footer.scss 	modified:   src/scss/blocks/header.scss 	modified:   src/scss/media.scss 	modified:   src/scss/style.scss

--- src/js/modules/input.js
+++ src/js/modules/input.js
@@ -1,6 +1,19 @@
 export function Input() {
     const inputFocus = $('.input__field-focus')
 
+    inputFocus.each(function() {
+      if($(this).val() !== '') {
+        let currentLabel = $(this).siblings('.input__label'),
+            currentValue = currentLabel.css('transition')
+
+        currentLabel.css('transition','none').addClass('active')
+
+        setTimeout(function() {
+            currentLabel.css('transition',currentValue)
+        }, 1)
+      }
+    })
+
     inputFocus.focusin( function() {
         $(this).siblings('.input__label').addClass('active')
     })
--- src/js/modules/menu.js
+++ src/js/modules/menu.js
@@ -1,5 +1,24 @@
 export function Menu() {
     $('#burger').click(function () {
-        $(this).toggleClass('active')
+        $(this).addClass('active')
+        $('.nav').addClass('active')
+        $('.overlay').stop().fadeIn('fast').addClass('active')
     })
+
+    $('.overlay').click(function () {
+        $(this).stop().fadeOut('fast')
+        $('#burger').removeClass('active')
+        $('.nav').removeClass('active')
+    })
+    // Скрипт, который скрывает и показывает хэдер при скролле вверх и вниз
+    let prevScrollpos = window.pageYOffset;
+    $(window).scroll(function () {
+        let currentScrollPos = window.pageYOffset;
+        if (prevScrollpos > currentScrollPos) {
+            $(".header").removeClass('hidden');
+        } else {
+            $(".header").addClass('hidden');
+        }
+        prevScrollpos = currentScrollPos;
+    });
 }
